89da340228d1e553a22e08c8f9ac4a26f75255b5
Hanut Singh <70129367+hanut1909@users.noreply.github.com>
2025-09-28 22:27:42 +0530

    Create Subtraction.py

---
+++ Subtraction.py
@@ -0,0 +1,11 @@
+# Program to subtract two numbers entered by the user
+
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the difference
+result = abs(num1 - num2)
+
+# Display the result
+print("The result of subtraction is:", result)

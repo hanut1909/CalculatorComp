cf86321adec378c8ef057706b754573edb48f184
Parag Bhattacharjee <psychoprg357@gmail.com>
2025-10-01 21:40:10 +0530

    Create sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,12 @@
+# Program to find square root of numbers entered by the user
+
+import math
+
+# Take input from the user
+num1 = float(input("Enter first number: "))
+
+# find square root
+result = math.sqrt(num1)
+
+# Display the result
+print("The square root is:", result)

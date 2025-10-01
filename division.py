c2d733acb6ac1a34ae269a8ac43553c7784f813e
Parag Bhattacharjee <psychoprg357@gmail.com>
2025-10-01 21:30:10 +0530

    Create division.py

---
+++ division.py
@@ -0,0 +1,11 @@
+# Program to divide two numbers entered by the user
+
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Divide num1 by num2
+result = num1 // num2
+
+# Display the result
+print("The result of division is:", result)
